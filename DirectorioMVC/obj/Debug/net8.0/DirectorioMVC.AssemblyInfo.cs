1b1c412fc23ccc0f109ebb420f4aca135b98e3a0
bryanm25s <143955938+bryanm25s@users.noreply.github.com>
2025-09-08 22:00:09 -0600

    s

--- DirectorioMVC/obj/Debug/net8.0/DirectorioMVC.AssemblyInfo.cs
+++ DirectorioMVC/obj/Debug/net8.0/DirectorioMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8320f1f542568fe835bd66e09c8e6765442c83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d52d3487c88912aa3b1aae0e1caf03f4cdc7d93")]
 [assembly: System.Reflection.AssemblyProductAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
